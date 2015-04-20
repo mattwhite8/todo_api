453596fecef96c8d84f22f7153c0e8d37e541c93
mattwhite8 <mattwhite1337@gmail.com>
2015-04-20 23:11:22 +0000

    added authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420161846) do
+ActiveRecord::Schema.define(version: 20150420171059) do
 
   create_table "items", force: true do |t|
     t.string   "title"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150420161846) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
+    t.string   "password"
   end
 
 end
